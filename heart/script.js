48c4c6d96b194981ff8bebc63da71eaaa02a9b80
Tuan Wannafly <111724358+tuanwannafree@users.noreply.github.com>
2025-02-14 03:10:49 +0700

    Update script.js

--- heart/script.js
+++ heart/script.js
@@ -31,7 +31,7 @@ function init() {
         container.addChild(heart);
     }
 
-    var text = new createjs.Text("No Valentine? No problem. This page loves you unconditionally. 🫶", "bold 24px Arial", "#312");
+    var text = new createjs.Text("Wait… you’re still here? Damn, we might be soulmates :)))", "bold 24px Arial", "#312");
     text.textAlign = "center";
     text.x = w / 2;
     text.y = h / 2 - text.getMeasuredLineHeight();
